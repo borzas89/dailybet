e69c16899232d0d18ccbf4a9ca1767017af8066e
borzas89 <borza.zsolt89@gmail.com>
2020-07-16 12:21:01 +0200

    adding components
    adding footer, navigation components

---
+++ src/app/component/navigation/navigation-user/navigation-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-user',
+  templateUrl: './navigation-user.component.html',
+  styleUrls: ['./navigation-user.component.scss']
+})
+export class NavigationUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
