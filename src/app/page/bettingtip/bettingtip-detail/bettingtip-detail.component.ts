d571adb485f761feb6ab1a5b86170ec931514cfd
borzas89 <borza.zsolt89@gmail.com>
2020-08-12 00:08:20 +0200

    small changes
    do not enable editing for everyone...

--- src/app/page/bettingtip/bettingtip-detail/bettingtip-detail.component.ts
+++ src/app/page/bettingtip/bettingtip-detail/bettingtip-detail.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Location} from '@angular/common';
 import {BettingtipService} from '../../../service/bettingtip.service';
+import {UserService} from '../../../service/user.service';
+import {Role} from '../../../model/role';
 
 @Component({
   selector: 'app-bettingtip-detail',
@@ -14,22 +16,28 @@ export class BettingtipDetailComponent implements OnInit {
   bettingTip: Bettingtip = new Bettingtip();
   item: Observable<Bettingtip>;
   selectId: string;
+  isEnabled = false;
 
   constructor(private route: ActivatedRoute,
               public  bettingtipService: BettingtipService,
+              private userService: UserService,
               private location: Location,
               private router: Router) { }
 
   ngOnInit() {
     const evId = this.route.snapshot.params['tippUid'];
-    console.log('tipuid ' + evId);
     this.selectId = evId;
-    console.log('1seleci' + this.selectId);
     const item = this.route.snapshot.params['id'];
     this.bettingTip = new Bettingtip();
     this.bettingtipService.findOneTippByUid(item).subscribe( data => {
       this.bettingTip = data;
     });
+
+    this.userService.currentUser.subscribe( data =>
+    this.isEnabled = data.role === Role.ADMIN
+    );
+
+
 
   }
 
