100684eca6a453aaaf07b887afec4249c9d81ff3
borzas89 <borza.zsolt89@gmail.com>
2020-09-26 16:04:49 +0200

    styling
    adding some css, add styles

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BettingtipService} from '../../service/bettingtip.service';
+import {Bettingtip} from '../../model/bettingtip';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  old_analysis: Array<Bettingtip>;
+  constructor(private bettingtipService: BettingtipService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.getDatas();
+  }
+  getDatas() {
+    this.bettingtipService.findOldTips().subscribe(data => {
+      this.old_analysis = data;
+    });
+  }
+
+  openTip(item) {
+    this.router.navigate(['/bettingtip/detail/' + item]);
   }
 
 }
